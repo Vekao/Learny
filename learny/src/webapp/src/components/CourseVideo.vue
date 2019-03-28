a5ed7c45c3cfe2d690ea55eee8d7567a69c869ce
Vekao <virginie.kernel@gmail.com>
2019-03-28 14:50:00 +0100

    front: scss modification

---
+++ learny/src/webapp/src/components/CourseVideo.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <!-- ajouter le lien youtube de la vidéo -->
+        <input type="text">
+        <button>Ajouter une vidéo</button>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
